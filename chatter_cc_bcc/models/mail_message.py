8155e58c4e988fa867ae4652dfeee0e65ca2d9bd
TopshengSilicone <sales@tssilicone.com>
2026-03-31 10:44:45 +0700

    Delete chatter_cc_bcc directory

--- chatter_cc_bcc/models/mail_message.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class MailMessage(models.Model):
-    _inherit = "mail.message"
-
-    email_cc = fields.Char(string="CC")
-    email_bcc = fields.Char(string="BCC")
